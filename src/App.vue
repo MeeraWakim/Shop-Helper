936068e3c5db45682ebf571e418c7c7e4ef0f3a7
MeeraWakim <meerawakim@utexas.edu>
2018-11-27 16:57:47 -0600

    made list that expands, now I'm trying to change the font sizee

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <v-app>
-
+  <!--<div v-bind:style="{fontSize: '50px' }">-->
+    <v-container fluid px-0>
+      <v-toolbar class="display-3"> Cart Buddy </v-toolbar>
+      <v-list>
+        <template v-for="(item, index) in items">
+     <v-layout align-center >
+        <v-checkbox  v-model="includeFiles" hide-details class="shrink mr-2"></v-checkbox>
+        <v-text-field class="display-4" height="200px"></v-text-field>
+      </v-layout>     
+        </template>
+      </v-list>
+      <v-icon large @click="addItem()">add_shopping_cart</v-icon>
+      
+    </v-container>
+    
   </v-app>
 </template>
 
@@ -16,11 +30,21 @@ export default {
 
     data() {
       return {
+        checkbox: false,
+        radioGroup: 1,
+        switch1: true,
         flaskdata: 'HelloBitches',
+        items: [
+          { header: 'Today' },
+          
+        ],
       }
     },
     methods: {    onClick(){
       this.flaskdata = "hi mom";
+    },
+    addItem(){
+      this.items.push(0);
     }
     },
     
