92fd55f83b32d276cfa0c080f479efa440cae613
Allison Welch <allisonwelch@utexas.edu>
2018-11-28 10:30:30 -0600

    autocomplete function added

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,20 @@
   <!--<div v-bind:style="{fontSize: '50px' }">-->
     <v-container fluid px-0>
       <v-toolbar class="display-3"> Cart Buddy </v-toolbar>
-      <v-list>
-        <template v-for="(item, index) in items">
-     <v-layout align-center >
-        <v-checkbox  v-model="includeFiles" hide-details class="shrink mr-2"></v-checkbox>
-        <v-text-field class="display-4" height="200px"></v-text-field>
-      </v-layout>     
-        </template>
-      </v-list>
+        <v-list>
+          <template v-for="(item, index) in items">
+            <v-layout align-center>
+            <v-checkbox  v-model="includeFiles" hide-details class="shrink mr-2"></v-checkbox>
+            
+            <v-autocomplete>
+            <v-text-field height="50px"></v-text-field>
+            </v-autocomplete>
+            
+            </v-layout>   
+          </template>
+        </v-list>
+        
+
       <v-icon large @click="addItem()">add_shopping_cart</v-icon>
       
     </v-container>
@@ -28,6 +34,10 @@ export default {
       
     },
 
+   
+ 
+
+
     data() {
       return {
         checkbox: false,
@@ -40,6 +50,7 @@ export default {
         ],
       }
     },
+
     methods: {    onClick(){
       this.flaskdata = "hi mom";
     },
