c62e27f17d9fe454e0c7117f833b8285a758bc3d
Allison Welch <allisonwelch@utexas.edu>
2018-11-30 06:58:05 -0600

    refining app, fixing bugs

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <v-app>
-      <v-toolbar class="display-2"> Cart Buddy <v-card class="title">item count: {{price}}</v-card></v-toolbar>
+      <v-toolbar class="display-1"> Cart Buddy <v-card class="title">item count: {{price}} <br/> total price: ${{currentTotal}}</v-card></v-toolbar>
       <v-flex sm5 >
       <v-card>
         <v-list>
-          <template v-for="(item, index) in itemsInList" v-bind:index="index" v-bind:price="price">
+          <template v-for="(item, foodIndex) in itemsInList" v-bind:foodIndex="foodIndex" v-bind:price="price">
             <v-layout align-center>
-              <v-checkbox v-model="checkBoxes[index]" hide-details class="shrink mr-2"></v-checkbox>
+              <v-checkbox v-model="checkBoxes[foodIndex]" hide-details class="shrink mr-2"></v-checkbox>
               <v-autocomplete
                 :items="grocery_database"
                 color="white"
                 item-text="name" 
-                v-model= "itemsInList[index]"
+                v-model= "itemsInList[foodIndex]"
               ></v-autocomplete>
-              <v-btn flat @click="clearItem(index)"><v-icon>clear</v-icon></v-btn>
+              <v-btn @click="submitItem()" color="success">Submit</v-btn>
+              <v-btn flat @click="clearItem(foodIndex)"><v-icon>clear</v-icon></v-btn>
             </v-layout>
           </template>
         </v-list>
@@ -34,7 +35,10 @@ export default {
 
     data() {
       return {
-        price:20,
+        
+        currentStrg: '',
+        currentRmvStrg: '',
+        price:1,
         grocery: '',
         checkbox: false,
         radioGroup: 1,
@@ -45,95 +49,136 @@ export default {
         model: null,
         search: null,
         grocery_database: [
-          { name: 'Apples', price: '1'},
-          { name: 'Asparagus', price: '2'},
-          { name: 'Avacados', price: '2'},
-          { name: 'Bacon', price: '5.5'},
-          { name: 'Bananas', price: '0.5'},
-          { name: 'Beef, Ground', price: '4'},
-          { name: 'Beer, Shiner Bock', price: '8'},
-          { name: 'Bread', price: '2.5'},
-          { name: 'Cheerios', price: '3'},
-          { name: 'Provolone Cheese', price: '3'},
-          { name: 'Cheez-its', price: '3'},
-          { name: 'Chicken Breast', price: '4'},
-          { name: 'Coffee', price: '5'},
-          { name: 'Dr Pepper', price: '3'},
-          { name: 'Eggs', price: '2'},
-          { name: 'Garlic', price: '1.5'},
-          { name: 'Grapes', price: '2'},
-          { name: 'Honey', price: '4'},
-          { name: 'Jello', price: '1'} ,  
-          { name: 'Ketchup', price: '3'},
-          { name: 'Mandarins', price: '5'},
-          { name: 'Mayonaise', price: '3'},
-          { name: 'Milk', price: '3.5'},
-          { name: 'Rice', price: '1'},
-          { name: 'Spaghetti', price: '1.5'},
-          { name: 'Spaghetti Sauce', price: '2'},
-          { name: 'Strawberries', price: '3'},
-          { name: 'Soup', price: '1'},
-          { name: 'Turkey', price: '3'},
-          { name: 'Yogurt', price: '1'} ],
+          'Apples',
+          'Asparagus',
+          'Avocado',
+          'Bacon',
+          'Bananas',
+          'Beef, Ground',
+          'Beer, Shiner Bock',
+          'Bread',
+          'Cheerios',
+          'Provolone Cheese',
+          'Cheez-its',
+          'Chicken Breast',
+          'Coffee',
+          'Dr Pepper',
+          'Eggs',
+          'Garlic',
+          'Grapes',
+          'Honey',
+          'Jello',  
+          'Ketchup',
+          'Mandarins',
+          'Mayonaise',
+          'Milk',
+          'Rice',
+          'Spaghetti',
+          'Spaghetti Sauce',
+          'Strawberries',
+          'Soup',
+          'Turkey',
+          'Yogurt'],
 
-        price_database:[
-          { price: '1'},
-          { price: '2'},
-          { price: '2'},
-          { price: '5.5'},
-          { price: '0.5'},
-          { price: '4'},
-          { price: '8'},
-          { price: '2.5'},
-          { price: '3'},
-          { price: '3'},
-          { price: '3'},
-          { price: '4'},
-          { price: '5'},
-          { price: '3'},
-          { price: '2'},
-          { price: '1.5'},
-          { price: '2'},
-          { price: '4'},
-          { price: '1'} ,  
-          { price: '3'},
-          { price: '5'},
-          { price: '3'},
-          { price: '3.5'},
-          { price: '1'},
-          { price: '1.5'},
-          { price: '2'},
-          { price: '3'},
-          { price: '1'},
-          { price: '3'},
-          { price: '1'}]  
+        currentTotal: 0,
         model: null,
         hasSaved: false,
+        priceindex: 0,
+        priceRemoveIndex: 0,
+        price_database:[
+         1,
+         2,
+         2,
+         5.50,
+         0.50,
+         4,
+         8,
+         2.50,
+         3,
+         3,
+         3,
+         4,
+         5,
+         3,
+         2,
+         1.50,
+         2,
+         4,
+         1 ,  
+         3,
+         5,
+         3,
+         3.50,
+         1,
+         1.50,
+         2,
+         3,
+         1,
+         3,
+         1],  
+
         itemsInList: [{}],
-        chosenItem: [{}],
         checkBoxes: [{}],
       }
     },
 
-    methods: {    
+    methods: {  
+      submitItem(){
+        for (var i = 0; i < this.itemsInList.length; i++) {
+          this.currentStrg = String(this.itemsInList[i]);
+          console.log("current string: " + this.currentStrg);
+          for (var q = 0; q < this.grocery_database.length; q++){
+            if (this.currentStrg === String(this.grocery_database[q])) {
+              
+              this.priceindex = q;
+              this.currentTotal += this.price_database[this.priceindex];
+            }
+          
+          console.log( "price database item: " + String(this.price_database[this.price.index]));
+          }
+        }
+
+        //console.log(this.priceIndex);
+        console.log("current total: " + this.currentTotal);
+        //console.log("price:" + this.price_database[this.priceindex]);
+        
+      }, 
       addItem(){
         this.itemsInList.push(0);
-        this.chosenItem.push(0);
         this.checkBoxes.push(0);
         this.price = this.itemsInList.length;
+
+        
+        } , 
+      
+
+      clearItem(foodIndex){
+        this.itemsInList.splice(foodIndex,1);
+        console.log("items in list: "+this.itemsInList);
+        this.price = this.itemsInList.length;
+        
+
+        for (var i = 0; i < this.itemsInList.length; i++) {
+          this.currentRmvStrg = String(this.itemsInList[i]);
+          console.log("current remove string: " + this.currentRmvStrg);
+          for (var q = 0; q < this.grocery_database.length; q++){
+            if (this.currentRmvStrg === String(this.grocery_database[q])) {
+              
+              this.priceRemoveIndex = q;
+              this.currentTotal -= this.price_database[this.priceRemoveIndex];
+            }
+            //console.log( "grocery database item: " + String(this.grocery_database[q]));
+          }
+        }
         
       },
-      clearItem(index){
-        this.itemsInList.splice(index,1);
-        console.log(this.itemsInList);
-      },
-      autocompleteAdded(){
-        console.log("autocomplete triggered");
-        console.log(this.price);
-      },
-    },
-  
 
+
+      // autocompleteAdded(){
+      //   console.log("I just triggered autocomplete");
+        // console.log(this.price);
+      // },
+    },
   };
 </script>
 
